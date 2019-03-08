bc90b505cf1251375f669b33aa37b6af48db82e3
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2019-03-08 16:00:50 +0200

    Cleaner code in App.js

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ const App = () => {
 
 	const onFetchData = (data) => {
 		const {tokenInput,dateInput} = data
+		const startDate = dateInput[0];
+		const endDate = dateInput[1]
 		setLoading(true)
 		setDataRes(null)
-		axios.get(`https://api.giosg.com/api/reporting/v1/rooms/84e0fefa-5675-11e7-a349-00163efdd8db/chat-stats/daily/?start_date=${dateInput[0]}&end_date=${dateInput[1]}`
+		axios.get(`https://api.giosg.com/api/reporting/v1/rooms/84e0fefa-5675-11e7-a349-00163efdd8db/chat-stats/daily/?start_date=${startDate}&end_date=${endDate}`
             , { headers: {"Authorization" : `Token ${tokenInput}`} })
             .then(res => {
 				const data = res.data
